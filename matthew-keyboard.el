113f01473918c3eeecef44127bbd553c7e0ed270
Matthew Brett <mb312@tom.bic.berkeley.edu>
2009-03-26 17:21:30 -0700

    Added my keyboard shortcuts, edits to initialization

---
+++ matthew-keyboard.el
@@ -0,0 +1,8 @@
+;; some keybindings I like
+(global-set-key [f1]      'goto-line)  
+(global-set-key [f2]      'longlines-mode) 
+(global-set-key [f8]      'flymake-mode)
+(global-set-key [f9]      'python-mode)
+(require 'doctest-mode)
+(global-set-key [f10]      'doctest-mode)
+(global-set-key [f11]      'rst-mode)
