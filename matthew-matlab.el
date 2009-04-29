651791b003a9d4c9b7ce750f18a893e00c49afcb
Matthew Brett <matthew.brett@gmail.com>
2009-04-29 14:47:09 -0700

    Matlab mode

---
+++ matthew-matlab.el
@@ -0,0 +1,12 @@
+;; matlab settings
+(autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
+(setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
+(defun my-matlab-mode-hook ()
+(setq matlab-indent-function t)	; if you want function bodies indented
+(setq fill-column 76)		; where auto-fill should wrap
+(turn-on-auto-fill))
+(setq matlab-mode-hook 'my-matlab-mode-hook)
+(autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
+(defun my-matlab-shell-mode-hook ()
+'())
+(setq matlab-mode-hook 'my-matlab-mode-hook)
