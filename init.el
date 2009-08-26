2b21c9f4c9d1e488ebad7700cde74edcd0bbf2f0
Matthew Brett <matthew.brett@gmail.com>
2009-08-26 13:23:06 -0700

    Remove funny font

--- init.el
+++ init.el
@@ -250,6 +250,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "gray7" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal :family "Consolas"))))
+ '(default ((t (:stipple nil :background "gray7" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 122 :width normal))))
  '(flymake-errline ((((class color)) (:background "DarkRed"))))
  '(flymake-warnline ((((class color)) (:background "DarkBlue")))))
